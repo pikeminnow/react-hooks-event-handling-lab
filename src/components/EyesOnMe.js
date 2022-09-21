c1893af3bb306457c7a39b814a83fe3e217caab4
Ashton MacKenzie <pikeminnow@gmail.com>
2022-09-21 11:42:25 -0400

    commit tree e64a1b6beb52cb420e71cfa58a4bfaa38f1fd510

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe (){
+    return (
+   
+            <button 
+            onFocus={() => console.log('Good!')} 
+            onBlur={() => console.log("Hey! Eyes on me!")}
+            >
+             Eyes on me 
+            </button>
+    )
+}
+
+export default EyesOnMe;
